8068c59595ded30cc2b5a49ddd8f884b213caf3c
og9288339-ops <og9288339@gmail.com>
2026-02-21 22:10:10 +0200

    Update CheckoutPage.js

--- project-root/frontend/src/pages/CheckoutPage.js
+++ project-root/frontend/src/pages/CheckoutPage.js
@@ -3,9 +3,11 @@ import Checkout from '../components/Checkout';
 
 const CheckoutPage = () => {
   return (
-    <div>
-      <h1>Checkout</h1>
-      <Checkout />
+    <div className="content">
+      <h1 style={{ marginBottom: '2rem', textAlign: 'center' }}>Checkout</h1>
+      <div style={{ maxWidth: '800px', margin: '0 auto' }}>
+        <Checkout />
+      </div>
     </div>
   );
 };

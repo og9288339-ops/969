26da8ca270ece72796aa1964c1a1d4c5cf71b0b5
og9288339-ops <og9288339@gmail.com>
2026-03-13 23:23:41 +0200

    Update stripe.js

--- backend/config/stripe.js
+++ backend/config/stripe.js
@@ -1,4 +1,22 @@
-const Stripe = require('stripe');
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+/**
+ * @module stripe
+ * @description Production-grade Stripe integration for luxury MERN e-commerce
+ * @author Senior Backend Architect
+ * @version 4.2.0
+ * @since 2024
+ */
 
-module.exports = stripe;
+import Stripe from 'stripe';
+import crypto from 'crypto';
+
+/**
+ * @typedef {Object} PaymentIntentData
+ * @property {string} orderId - Unique order ID
+ * @property {string} userId - User ID
+ * @property {number} amount - Amount in cents
+ * @property {string} currency - Currency code (usd, egp, eur)
+ * @property {Object} metadata - Additional metadata
+ */
+
+/**
+ * Stripe instance

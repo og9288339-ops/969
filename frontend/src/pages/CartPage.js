684420230cdfdca58e1431811a0f588b730d4dc6
og9288339-ops <og9288339@gmail.com>
2026-03-13 23:07:12 +0200

    Update CartPage.js

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,18 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Cart from '../components/Cart';
-
-const CartPage = () => {
-  return (
-    <div>
-      <h1>Shopping Cart</h1>
-      <Cart />
-      <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-        <Link to="/products" className="btn">Continue Shopping</Link>
-        <Link to="/checkout" className="btn" style={{ marginLeft: '1rem' }}>Proceed to Checkout</Link>
-      </div>
-    </div>
-  );
-};
-
-export default CartPage;
+/**
+ * @module CartPage
+ * @description World-class luxury e-commerce cart page
+ * @author Senior UI/UX Architect
+ * @version 2.1.0
+ * @since

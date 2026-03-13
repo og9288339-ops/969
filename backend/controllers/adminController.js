7b4f2c2611b8da60a34784c4ac4e377c3d1b1bad
og9288339-ops <og9288339@gmail.com>
2026-03-13 23:25:43 +0200

    Update adminController.js

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,41 +1,9 @@
-const User = require('../models/User');
-const Product = require('../models/Product');
-const Order = require('../models/Order');
+/**
+ * @module adminController
+ * @description Senior-level admin API controller for luxury MERN e-commerce
+ * @author Senior Backend Architect
+ * @version 4.0.0
+ * @since 2024
+ */
 
-exports.getUsers = async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-exports.getProducts = async (req, res) => {
-  try {
-    const products = await Product.find().populate('seller');
-    res.json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-exports.getOrders = async (req, res) => {
-  try {
-    const orders = await Order.find().populate('user products.product');
-    res.json(orders);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-exports.analytics = async (req, res) => {
-  try {
-    const totalUsers = await User.countDocuments();
-    const totalProducts = await Product.countDocuments();
-    const totalOrders = await Order.countDocuments();
-    res.json({ totalUsers, totalProducts, totalOrders });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+import User

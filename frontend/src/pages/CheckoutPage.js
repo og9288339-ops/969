054584f899c94fdaec36c94e44d3c129bf92bac7
og9288339-ops <og9288339@gmail.com>
2026-03-13 23:09:05 +0200

    Update CheckoutPage.js

--- frontend/src/pages/CheckoutPage.js
+++ frontend/src/pages/CheckoutPage.js
@@ -1,13 +1,4 @@
-import React from 'react';
-import Checkout from '../components/Checkout';
-
-const CheckoutPage = () => {
-  return (
-    <div>
-      <h1>Checkout</h1>
-      <Checkout />
-    </div>
-  );
-};
-
-export default CheckoutPage;
+/**
+ * @module CheckoutPage
+ * @description World-class luxury e-commerce checkout experience
+ * @author Senior

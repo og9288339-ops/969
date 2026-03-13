9fb251d2fd07fb75e1a9c0fad609cf50fe1b8a06
og9288339-ops <og9288339@gmail.com>
2026-03-13 23:12:52 +0200

    Update NotFound.js

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,14 +1,4 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const NotFound = () => {
-  return (
-    <div style={{ textAlign: 'center', padding: '4rem' }}>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you're looking for doesn't exist.</p>
-      <Link to="/" className="btn">Go Home</Link>
-    </div>
-  );
-};
-
-export default NotFound;
+/**
+ * @module NotFound
+ * @description World-class 404 error page for luxury e-commerce
+ * @author Senior
